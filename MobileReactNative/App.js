91c7ef4e3b9699b642f975d0b5c8f0cecfdd4ad4
Butiri Cristian <butiri.cristian@gmail.com>
2017-10-21 13:44:56 +0300

    Added app description

--- MobileReactNative/App.js
+++ MobileReactNative/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text></Text>
+        <View style={{flexDirection: 'row'}}>
+          <TextInput style = {{flex: 0.8, height: 40}}/>
+        </View>
       </View>
     );
   }
